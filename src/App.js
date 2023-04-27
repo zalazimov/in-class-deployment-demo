95b3e72bf1c5d08bc769ac050cb134971ecc9658
zazimov <zalmanazimov@pursuit.org>
2023-04-27 12:18:38 -0400

    added .env file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Simple React App</div>;
+  console.log(process.env);
+
+  return (
+    <div className="App">
+      Simple React App
+      <div>Current Env: {process.env.NODE_ENV}</div>
+      <div>
+        Current API KEY:{" "}
+        {process.env.NODE_ENV === "development"
+          ? process.env.REACT_APP_DEVELOPMENT_API_KEY
+          : process.env.REACT_APP_PRODUCTION}
+      </div>
+    </div>
+  );
 }
 
 export default App;
